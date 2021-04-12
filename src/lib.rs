8f76b5590d5141c50fcaeac70e135e62879b1072
Konrad Siek <konrad.siek@gmail.com>
2021-04-12 17:42:20 +0200

    Smaller query for toy datasets.

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@ use djanco::csv::*;
 
 use djanco_ext::*;
 
-#[djanco(April, 2020, subsets(SmallProjects))]
+#[djanco(May, 2021, subsets(All))]
 pub fn my_query(database: &Database, _log: &Log, output: &Path) -> Result<(), std::io::Error>  {
     database.projects()
         .group_by(project::Language)
         .sort_by(project::Stars)
-        .sample(Top(1000))
-        .into_csv_in_dir(output, "top_1000_small_projects_by_stars.csv")
+        .sample(Top(1))
+        .into_csv_in_dir(output, "top_1_project_by_stars_in_each_language.csv")
 }
